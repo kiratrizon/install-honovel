1c2f3011f726935297aedf8620e334a794945222
kyuuukira <genesistroy.fdc@gmail.com>
2025-06-19 18:39:33 +0800

    install

--- routes/web.ts
+++ routes/web.ts
@@ -3,4 +3,10 @@ import Route from "Illuminate/Support/Facades/Route";
 Route.get("/", async (_) => {
   return "Welcome to the Deno Honovel Installer!";
 });
+
+Route.get("/install", async (_) => {
+  return response().file(basePath("install.ts")).withHeaders({
+    "Content-Type": "application/typescript",
+  });
+});
 export default Route;
